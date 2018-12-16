c1c6eee4e182c357e3f604723b23690fa29025eb
Tathagata Basu <34139584+tathagatabasu@users.noreply.github.com>
2018-12-17 00:45:00 +0530

    added package documentation

---
+++ R/package.R
@@ -0,0 +1,16 @@
+#' bootlasso: This package is for computation with LASSO
+#'
+#' This is a full frequentist package for based on LASSO.
+#' There are several functions in this package for use. The
+#' package is based on three different optimization techniques
+#' for non-differentiable functions. The package can work is
+#' enabled for bootstrap and cross-validation method for LASSO.
+#' Besides these, it can be used for checking sensitivity of a 
+#' predictor using adaptive(weighted) LASSO.
+#'
+#' @section bootlasso functions:
+#' Use ls("package:bootlasso") to check the available functions.
+#'
+#' @docType package
+#' @name bootlasso
+NULL
